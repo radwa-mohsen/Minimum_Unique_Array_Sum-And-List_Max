9d26ed3869cfc3ca0999328aa21022960196b6d5
Venkateswara Gupta Pola <43083074+venkateswarapola@users.noreply.github.com>
2019-03-31 17:25:38 +0530

    E2,E3

--- minimum_unique_array.cpp
+++ minimum_unique_array.cpp
@@ -1,26 +1,22 @@
-#include <iostream> 
-using namespace std;  
-int minSum(int arr[], int n) 
-{ 
-    int sum = arr[0]; 
-  
-    for (int i = 1; i < n; i++) { 
-        if (arr[i] == arr[i - 1]) {              
-            int j = i; 
-            while (j < n && arr[j] <= arr[j - 1]) {           
-                arr[j] = arr[j] + 1; 
-                j++; 
-            } 
-        } 
-         sum = sum + arr[i]; 
-     } 
-  
-    return sum; 
-} 
-int main() 
-{ 
-    int arr[] = { 2, 2, 3, 5, 6 }; 
-    int n = sizeof(arr) / sizeof(arr[0]); 
-    cout << minSum(arr, n) << endl; 
-    return 0; 
-} 
+static int minUniqueSum(int[] A) {
+    int n = A.length;
+
+
+    int sum = A[0];
+    int prev = A[0];
+
+    for( int i = 1; i < n; i++ ) {
+        int curr = A[i];
+
+        if( prev == curr ) {
+            curr = curr+1;
+            sum += curr;
+        }
+        else {
+            sum += curr;
+        }
+        prev = curr;
+    }
+
+    return sum;
+}
